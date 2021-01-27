ba9fb2440bda512beb9a98492bdcb33e6535a082
Gregory Innovo <gregoryinnovo@gmail.com>
2021-01-27 17:20:28 -0500

    Se agrego nueva Routa: login

---
+++ src/containers/Login.jsx
@@ -0,0 +1,37 @@
+import React, { Fragment } from 'react';
+import '../assets/styles/components/Login.scss';
+import googleIcon from '../assets/static/google-g-icon-9.jpg';
+import TwitterIcon from '../assets/static/Twitter_logo_white.png';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+const Login = () => (
+    <Fragment>
+
+    <Header />
+    <section className="login">
+        <section className="login__container">
+            <h2>Inicia Sesión</h2>
+            <form className="login__container--form">
+                <input aria-label="Correo" className="input__login" type="text" placeholder="Correo" />
+                <input aria-label="Contraseña" className="input__login" type="password" placeholder="Contraseña" />
+                <button className="button">Iniciar sesión</button>
+                <div className="login__container--remember-me">
+                    <label>
+                        <input type="checkbox" id="cbox1" value="checkbox" />Recuérdame
+                    </label>
+                    <a href="#">Olvidé mi contraseña</a>
+                </div>
+            </form>
+            <section className="login__container--social-media">
+                <div><img src={googleIcon} alt="Google" />Inicia sesión con Google</div>
+                <div><img src={TwitterIcon} alt="Twitter" />Inicia sesión con Twitter</div>
+            </section>
+            <p className="login__container--register">No tienes nunguna cuenta <a href="registro.html">Regístrate</a></p>
+        </section>
+    </section>
+    <Footer />
+    </Fragment>
+);
+
+export default Login;
--- webpack.config.js
+++ webpack.config.js
@@ -47,7 +47,10 @@ module.exports = {
           }
         ]
       }
-    ],
+    ]
+  },
+  devServer: {
+    historyApiFallback: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
