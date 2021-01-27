313b56bc5800d86598d261de26ffda79a9c80f7b
Gregory Innovo <gregoryinnovo@gmail.com>
2021-01-26 21:02:48 -0500

    Json y contenido

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -3,20 +3,22 @@ import '../assets/styles/components/CarouselItem.scss';
 import playIcon from '../assets/static/baseline_play_arrow_white_18dp.png';
 import starIcon from '../assets/static/baseline_grade_white_18dp.png';
 
-const CarouselItem = () => (
+const CarouselItem = ({cover, title, year, contentRating, duration}) => (
 
     <div className="carousel-item">
         <img className="carousel-item__img"
-            src="https://images.pexels.com/photos/89778/strawberries-frisch-ripe-sweet-89778.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-            alt="Fresas"/>
+            src={cover}
+            alt={title}/>
         <div className="carousel-item__details">
             <div>
                 <img src={playIcon} alt="Play" />
                 <img src={starIcon} alt="Star" />
             </div>
 
-            <p className="carousel-item__details--title">Fresas</p>
-            <p className="carousel-item__details--subtitle">Frutas</p>
+            <p className="carousel-item__details--title">{title}</p>
+            <p className="carousel-item__details--subtitle">
+                {`${year} ${contentRating} ${duration}`}
+            </p>
         </div>
     </div>
 );
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,41 +1,49 @@
-import React, { Fragment } from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../components/Header';
 import Search from '../components/Search';
-import '../assets/styles/App.scss';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
 import Footer from '../components/Footer';
+import '../assets/styles/App.scss';
 
-const App = () => (
-    <div className="App">
-        <Header />
-        <Search />
+const App = () => {
+    const [videos, setVideos] = useState({ mylist: [], trends: [], originals: [] });
+    useEffect(() => {
+        fetch('http://localhost:3000/initialState')
+         .then(response => response.json())
+         .then(data => setVideos(data))
+    }, []);
 
-        <Categories title="Frutas">
-            <Carousel>
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-            </Carousel>
-        </Categories>
+    return(
+        <div className="App">
+            <Header />
+            <Search />
 
-        <Categories title="Nuevos">
-            <Carousel>
-                <CarouselItem />
-                <CarouselItem />
-            </Carousel>
-        </Categories>
+            {videos.mylist.length > 0 && 
+                <Categories title="Frutas">
+                    <Carousel>
+                        <CarouselItem />
+                    </Carousel>
+                </Categories>
+            }
 
-        <Categories title="Próximamente">
-            <Carousel>
-                <CarouselItem />
-            </Carousel>
-        </Categories>
+            <Categories title="Nuevos">
+                <Carousel>
+                    {videos.trends.map(item =>
+                        <CarouselItem key={item.id} {...item}/>
+                    )}
+                </Carousel>
+            </Categories>
 
-        <Footer />
-    </div>
-);
+            <Categories title="Próximamente">
+                <Carousel>
+                    <CarouselItem />
+                </Carousel>
+            </Categories>
 
+            <Footer />
+        </div>
+    );
+}
 export default App;
