970bffccbcf9acd9480862eaa79fa380b2ecf4f4
Gregory Innovo <gregoryinnovo@gmail.com>
2021-01-29 17:44:31 -0500

    Logout action

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import gravatar from '../utils/gravatar';
+import { logoutRequest } from '../actions';
+import PropTypes, { string } from 'prop-types';
 import '../assets/styles/components/Header.scss';
 import logo from '../assets/static/logo-foodhy.png';
 import userIcon from '../assets/static/user-icon.png';
@@ -10,6 +12,11 @@ const Header = props => {
     
     const { user } = props;
     const hasUser = Object.keys(user).length > 0;
+    
+    const handleLogout = () => {
+        // Reinicio el estado
+        props.logoutRequest({})
+    };
 
     return(
         <header className="header">
@@ -24,27 +31,45 @@ const Header = props => {
                     <img src={gravatar(user.email)} alt={user.email} />
                     :
                     <img src={userIcon} alt="Imagen de Perfil" />
-            
-
                 }
                     <p>Perfil</p>
                 </div>
                 <ul>
-                    <li><a href="/#">Cuenta</a></li>
-                    <li>
-                        <Link to="/login">
-                            Iniciar Sesión
-                        </Link>
-                    </li>
+
+                    {hasUser ? 
+                        <li><a href="/#">{user.name}</a></li>
+                        :null
+                    }
+
+                    {hasUser ? 
+                        <li><a href="#logout" onClick={handleLogout}>Cerrar Sesión</a></li>
+                        :
+                        <li>
+                            <Link to="/login">
+                                Iniciar Sesión
+                            </Link>
+                        </li>    
+                    }
                 </ul>
                 
             </div>
 
         </header>
     )};
+
+Header.propTypes = {
+    user: PropTypes.object.isRequired,
+    logoutRequest: PropTypes.func.isRequired,
+}
+
 const mapStateToProps = state => {
     return {
         user: state.user,
     }
 }
-export default connect(mapStateToProps, null) (Header);
+
+const mapDispatchToProps = {
+    logoutRequest,
+}
+
+export default connect(mapStateToProps, mapDispatchToProps) (Header);
