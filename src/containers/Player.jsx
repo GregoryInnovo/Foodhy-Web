7b7535e7c516f1a7a3a75fcdd30e11cfe96127ba
Gregory Innovo <gregoryinnovo@gmail.com>
2021-01-29 23:40:58 -0500

    Playing state

--- src/containers/Player.jsx
+++ src/containers/Player.jsx
@@ -1,21 +1,46 @@
-import React from 'react';
+import React, { useLayoutEffect, useState } from 'react';
+import { connect } from 'react-redux';
+import NotFound from '../containers/NotFound';
+import { getVideoSource } from '../actions';
 import '../assets/styles/components/Player.scss';
-import { Link } from 'react-router-dom';
-const Player = () => {
-    return (
+import { Redirect } from 'react-router-dom';
+
+const Player = props => {
+    const { id } = props.match.params;
+    const hasPlaying = Object.keys(props.playing).length > 0;
+    // const [loading, setLoading] = useState(true);
+
+    useLayoutEffect(() => {
+        props.getVideoSource(id);
+        // setLoading(false);
+    }, [])
+
+    // if(loading)
+    //     return <h2>Cargando...</h2>
+
+
+    return hasPlaying ? (
         <div className="Player">
-            <video controls autoplay>
-                <source src="" type="video/mp4"/>
+            <video muted controls autoPlay>
+                <source src={props.playing.source} type="video/mp4"/>
             </video>
             <div className="Player-back">
-                <Link to="/">
-                    <button type="button"> 
-                        Regresar
-                    </button>
-                </Link>
+                <button type="button" onClick={() => props.history.goBack()}> 
+                    Regresar
+                </button>
             </div>
         </div>
-    );
+    ) : setTimeout(<Redirect to='/404/' />, 0);
 };
 
-export default Player;
+const mapStateToProps = state =>{
+    return{
+        playing: state.playing,
+    }
+};
+
+const mapDispatchToProps = {
+    getVideoSource,
+};
+    
+export default connect(mapStateToProps, mapDispatchToProps) (Player);
